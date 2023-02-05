b0e21d1192ebf843b58e67492d4edf18d7faadba
lukasz.keska <lukasz.keska@gmail.com>
2023-02-05 20:50:05 +0100

    chore: changed templatemonster/react/templezen basename path

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { Suspense, useLayoutEffect } from 'react'
-import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
+import React, { Suspense, useLayoutEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter,
+} from "react-router-dom";
 
 // Homepages
 const Home = React.lazy(() => import("./components/pages/Home"));
 const Hometwo = React.lazy(() => import("./components/pages/Hometwo"));
 // Sermon
 const Sermon = React.lazy(() => import("./components/pages/Sermon"));
-const Sermondetails = React.lazy(() => import("./components/pages/Sermondetails"));
+const Sermondetails = React.lazy(() =>
+  import("./components/pages/Sermondetails")
+);
 // Event
 const Event = React.lazy(() => import("./components/pages/Event"));
-const Eventdetails = React.lazy(() => import("./components/pages/Eventdetails"));
+const Eventdetails = React.lazy(() =>
+  import("./components/pages/Eventdetails")
+);
 // Pages(About)
 const About = React.lazy(() => import("./components/pages/About"));
 const Contact = React.lazy(() => import("./components/pages/Contact"));
@@ -31,54 +40,144 @@ const Blogdetails = React.lazy(() => import("./components/pages/Blogdetails"));
 // Scroll to Top
 const ScrollToTop = withRouter(({ children, location: { pathname } }) => {
   useLayoutEffect(() => {
-    window.scrollTo(0, 0)
-  }, [pathname])
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-  return children || null
-})
+  return children || null;
+});
 
 function App() {
   return (
-    <Router basename={"/templatemonster/react/templezen/"}>
+    <Router basename={"/"}>
       <Suspense fallback={<div></div>}>
         <ScrollToTop>
           <Switch>
-            {/* Home */} 
+            {/* Home */}
             <Route exact path="/" component={Home} />
             <Route exact path="/home-v2" component={Hometwo} />
             {/* Sermon */}
-            <Route exact path="/sermon/tag/:tagId" component={props => (<Sermon {...props} key={window.location.pathname} />)} />
-            <Route exact path="/sermon/author/:authorId" component={props => (<Sermon {...props} key={window.location.pathname} />)} />
+            <Route
+              exact
+              path="/sermon/tag/:tagId"
+              component={(props) => (
+                <Sermon {...props} key={window.location.pathname} />
+              )}
+            />
+            <Route
+              exact
+              path="/sermon/author/:authorId"
+              component={(props) => (
+                <Sermon {...props} key={window.location.pathname} />
+              )}
+            />
             <Route exact path="/sermon" component={Sermon} />
-            <Route exact path="/sermon-details/:id" component={props => (<Sermondetails {...props} key={window.location.pathname} />)} />
+            <Route
+              exact
+              path="/sermon-details/:id"
+              component={(props) => (
+                <Sermondetails {...props} key={window.location.pathname} />
+              )}
+            />
             {/* Event */}
             <Route exact path="/event" component={Event} />
-            <Route exact path="/event-details/:id" component={props => (<Eventdetails {...props} key={window.location.pathname} />)} />
+            <Route
+              exact
+              path="/event-details/:id"
+              component={(props) => (
+                <Eventdetails {...props} key={window.location.pathname} />
+              )}
+            />
             {/* Pages(About) */}
             <Route exact path="/about" component={About} />
             <Route exact path="/contact" component={Contact} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/donation" component={Donation} />
-            <Route exact path="/team-details/:id" component={props => (<Teamdetails {...props} key={window.location.pathname} />)} />
+            <Route
+              exact
+              path="/team-details/:id"
+              component={(props) => (
+                <Teamdetails {...props} key={window.location.pathname} />
+              )}
+            />
             {/* Shop */}
-            <Route exact path="/shop/cat/:catId" component={props => (<Shop {...props} key={window.location.pathname} />)} />
-            <Route exact path="/shop/tag/:tagId" component={props => (<Shop {...props} key={window.location.pathname} />)} />
-            <Route exact path="/shop/search/:query" component={props => (<Shop {...props} key={window.location.pathname} />)} />
-            <Route exact path="/shop/:minPrice/:maxPrice" component={props => (<Shop {...props} key={window.location.pathname} />)} />
+            <Route
+              exact
+              path="/shop/cat/:catId"
+              component={(props) => (
+                <Shop {...props} key={window.location.pathname} />
+              )}
+            />
+            <Route
+              exact
+              path="/shop/tag/:tagId"
+              component={(props) => (
+                <Shop {...props} key={window.location.pathname} />
+              )}
+            />
+            <Route
+              exact
+              path="/shop/search/:query"
+              component={(props) => (
+                <Shop {...props} key={window.location.pathname} />
+              )}
+            />
+            <Route
+              exact
+              path="/shop/:minPrice/:maxPrice"
+              component={(props) => (
+                <Shop {...props} key={window.location.pathname} />
+              )}
+            />
             <Route exact path="/shop" component={Shop} />
             <Route exact path="/shop-left" component={Shopleft} />
             <Route exact path="/shop-right" component={Shopright} />
-            <Route exact path="/shop-details/:id" component={props => (<Shopdetails {...props} key={window.location.pathname} />)} />
+            <Route
+              exact
+              path="/shop-details/:id"
+              component={(props) => (
+                <Shopdetails {...props} key={window.location.pathname} />
+              )}
+            />
             <Route exact path="/cart" component={Cart} />
             <Route exact path="/checkout" component={Checkout} />
             {/* Blog */}
-            <Route exact path="/blog/cat/:catId" component={props => (<Blog {...props} key={window.location.pathname} />)} />
-            <Route exact path="/blog/tag/:tagId" component={props => (<Blog {...props} key={window.location.pathname} />)} />
-            <Route exact path="/blog/author/:authorId" component={props => (<Blog {...props} key={window.location.pathname} />)} />
-            <Route exact path="/blog/search/:query" component={props => (<Blog {...props} key={window.location.pathname} />)} />
+            <Route
+              exact
+              path="/blog/cat/:catId"
+              component={(props) => (
+                <Blog {...props} key={window.location.pathname} />
+              )}
+            />
+            <Route
+              exact
+              path="/blog/tag/:tagId"
+              component={(props) => (
+                <Blog {...props} key={window.location.pathname} />
+              )}
+            />
+            <Route
+              exact
+              path="/blog/author/:authorId"
+              component={(props) => (
+                <Blog {...props} key={window.location.pathname} />
+              )}
+            />
+            <Route
+              exact
+              path="/blog/search/:query"
+              component={(props) => (
+                <Blog {...props} key={window.location.pathname} />
+              )}
+            />
             <Route exact path="/blog" component={Blog} />
-            <Route exact path="/blog-details/:id" component={props => (<Blogdetails {...props} key={window.location.pathname} />)} />
+            <Route
+              exact
+              path="/blog-details/:id"
+              component={(props) => (
+                <Blogdetails {...props} key={window.location.pathname} />
+              )}
+            />
           </Switch>
         </ScrollToTop>
       </Suspense>
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/react-owl-carousel2/lib/styles.css';
-import '../node_modules/slick-carousel/slick/slick.css';
-import '../node_modules/magnific-popup/dist/magnific-popup.css';
-import './assets/css/font-awesome.css';
-import './assets/css/style.css';
-import './assets/css/responsive.css';
-import './index.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/react-owl-carousel2/lib/styles.css";
+import "../node_modules/slick-carousel/slick/slick.css";
+import "../node_modules/magnific-popup/dist/magnific-popup.css";
+import "./assets/css/font-awesome.css";
+import "./assets/css/style.css";
+import "./assets/css/responsive.css";
+import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter basename={"/templatemonster/react/templezen/"}>
+  <BrowserRouter basename={"/"}>
     <App />
   </BrowserRouter>,
-  document.getElementById('templezen')
+  document.getElementById("templezen")
 );
 
 // If you want to start measuring performance in your app, pass a function
